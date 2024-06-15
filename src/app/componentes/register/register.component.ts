e3b2a7bb0dd84017f5be4b2fe119853ccc2c1231
erickabunay <ericka.bunayn@ug.edu.ec>
2024-06-15 18:57:42 -0500

    roles

--- src/app/componentes/register/register.component.ts
+++ src/app/componentes/register/register.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from '../../app.component';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterModule,RouterModule,AppComponent],
+  imports: [RouterModule,RouterModule,AppComponent, FormsModule ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  title = 'ProyectoGrupo8';
+
+ 
+  public rol: string = '';
+
+  constructor() {
+   
+    this.rol = '';
+  }
 
 }
