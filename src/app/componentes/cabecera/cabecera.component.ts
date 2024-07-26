9f4f6df79dfdb112639220bac749b1abc5f39dff
Ericka <ameza@kachin.ec>
2024-07-25 22:50:18 -0500

    Se mejoro la cabecera

--- src/app/componentes/cabecera/cabecera.component.ts
+++ src/app/componentes/cabecera/cabecera.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../../app.component';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
+import { AuthServiceService } from '../../servicios/auth-service.service';
+
 @Component({
   selector: 'app-cabecera',
   standalone: true,
-  imports: [AppComponent,RouterOutlet,RouterModule],
+  imports: [AppComponent,RouterOutlet,RouterModule,CommonModule],
   templateUrl: './cabecera.component.html',
   styleUrl: './cabecera.component.css'
 })
-export class CabeceraComponent {
+export class CabeceraComponent implements OnInit {
+  isLoggedIn: boolean = false;
 
+  constructor(private authService: AuthServiceService, private router: Router) {}
+
+  ngOnInit() {
+    this.authService.isLoggedIn$.subscribe(status => {
+      this.isLoggedIn = status;
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/signin']);
+  }
 }
+
