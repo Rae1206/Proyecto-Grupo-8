c98e6bde1b11542096abf98cebae505057a59fa1
Ericka <ameza@kachin.ec>
2024-07-25 22:53:22 -0500

    Se agregó la interface de usuario

---
+++ src/app/Interfaces/usuario.ts
@@ -0,0 +1,12 @@
+export interface usuario
+{
+    Name : string,
+    LastName : string,
+    DateOfBirth : Date,
+    Gender : string,
+    Email : string,
+    Password : string,
+    Role : string,
+    IsDisable : boolean;
+
+}
