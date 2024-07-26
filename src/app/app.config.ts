4c2d8fb6dee057a9a1ddeb609e2fac506e8ab612
Ericka <ameza@kachin.ec>
2024-07-25 22:53:09 -0500

    Se importo el provideHttpClient para establecer la conexión a la API mediante el protocolo HTTP

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(),
+    FormsModule
+  ]
 };
