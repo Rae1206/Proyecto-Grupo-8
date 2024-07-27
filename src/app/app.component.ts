521802b4da4f9646cf6f1f1a1bedcf00b3b5e2dd
Ericka <ameza@kachin.ec>
2024-07-26 23:28:38 -0500

    Se añadio Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
 import { PiePaginaComponent } from './pie-pagina/pie-pagina.component';
 import { UserService } from './servicios/user.service'; // Ajusta la ruta según sea necesario
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button'; // Si usas botones
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CabeceraComponent, PiePaginaComponent, HttpClientModule],
+  imports: [RouterOutlet, CabeceraComponent, PiePaginaComponent, HttpClientModule
+    ,MatDialogModule,MatButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,13 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(),
-    FormsModule
+    FormsModule, provideAnimationsAsync()
   ]
 };
